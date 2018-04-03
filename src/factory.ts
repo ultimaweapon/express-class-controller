e64f04edeaac2acd9afa7636e9609895a2405bf6
Putta Khunchalee <ultimaweapon@outlook.com>
2018-04-03 20:47:48 +0700

    Refactor factory to supports more than request handler

--- src/factory.ts
+++ src/factory.ts
@@ -1,18 +1,35 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express'
 
-export type ActionProvider<T> = (controller: T) => RequestHandler
+/**
+ * The callback to provide a method to handle requests.
+ */
+export type RequestHandlerSelector<T> = (controller: T) => RequestHandler
 
+/**
+ * Base class for controllers factory.
+ */
 export abstract class Factory {
-  public resolve<T>(action: ActionProvider<T>): RequestHandler {
-    return (req, res, next) => this.executeAction(action, req, res, next)
+  /**
+   * Get a request handler for passing to Express.
+   *
+   * @param selector A call back to provide a method to handler requests.
+   */
+  public requestHandler<T>(selector: RequestHandlerSelector<T>): RequestHandler {
+    return (req, res, next) => this.executeRequestHandler(selector, req, res, next)
   }
 
+  /**
+   * Create a new controller to handle request.
+   */
   protected abstract createController<T>(): T;
 
-  private executeAction<T>(action: ActionProvider<T>, req: Request, res: Response, next: NextFunction): any {
+  private executeRequestHandler<T>(
+    selector: RequestHandlerSelector<T>,
+    req: Request,
+    res: Response,
+    next: NextFunction): any {
     let controller = this.createController<T>();
-    let handler = action(controller)
-
+    let handler = selector(controller)
     return handler(req, res, next)
   }
 }
